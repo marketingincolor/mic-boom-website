e78e6048ca396b440a8374ca9b5db34409a726d7
Adam Doe <adoe@marketingincolor.com>
2016-12-09 15:14:01 -0500

    Add Images and filename changes

---
+++ results-old_results.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en"><?php include 'create-queries.php'; ?>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta content="regularly" property="step6:value">
+    <title>Marketing Automation Game - Dynamic Content</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.css" />
+    <?php include 'page-includes.php'; ?>
+    <script src="generatecontent.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        var _ss = _ss || [];
+        _ss.push(['_setDomain', 'https://koi-UUHGVW.marketingautomation.services/net']);
+        _ss.push(['_setAccount', 'KOI-YU6QP0']);      
+        if (callThisOnReturn) {_ss.push(['_setResponseCallback', callThisOnReturn]);}
+        _ss.push(['_trackPageView']);
+        (function() {
+            var ss = document.createElement('script');
+            ss.type = 'text/javascript'; ss.async = true;
+            ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-UUHGVW.marketingautomation.services/client/ss.js?ver=1.1.1';
+            var scr = document.getElementsByTagName('script')[0];
+            scr.parentNode.insertBefore(ss, scr);
+        })();
+    </script>
+</head>
+<body class="boomcity">
+    <div class="header">
+        <div class="row">
+	        <div class="small-10 medium-4 small-centered columns">
+                <img src="./images/mic-bc-grfx-main-hdr.png" />
+            </div>
+        </div>
+    </div>
+
+    <div class="interact">
+        <div class="row">
+            <div class="small-12 medium-6 large-10 small-centered columns">
+                <div class="content">
+                    <h3 id="dynamicContentIDX" class="">Your Boom City company is<br>&nbsp;.<br></h3>
+                    <div class="small-6 small-offset-3">
+                        <a id="dynamicContentClick" href="./results-details"><img id="click-here-cloud" src="./images/click-here-cloud.png"></a>
+                    </div>
+                    <h5 id="dynamicContentIDY" style="color: #6e727f !important;" class="small-10 medium-8 columns small-centered">to see how its owner, &nbsp;, got his business booming.<br></h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="results-main">
+        <div class="row medium-collapse">
+            <div class="small-12 small-centered columns">
+                <div class="overlay bgnd-results-beta show-for-small-only"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div class="overlay bgnd-results show-for-medium-up"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div class="overlay start off"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+
+                <div id="type_alpha" class="overlay type two feta"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="type_beta" class="overlay type two epsilon"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="company_name" class="overlay busname">
+                    <img src="./images/mic-bc-grfx-main-clear-name-small.png" class="show-for-small-only">
+                    <img src="./images/mic-bc-grfx-main-clear-name.png" class="show-for-medium-up" />
+                    <div id="dynamicContentID0">&nbsp;</div>
+                </div>
+                <div id="team_alpha" class="overlay team three alpha"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="team_beta" class="overlay team three beta"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="crm_alpha" class="overlay crm four alpha"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="crm_beta" class="overlay crm four beta"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="online_alpha" class="overlay online five alpha"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="online_beta" class="overlay online five beta"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="frequency_alpha" class="overlay frequency six alpha off"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="owner_alpha" class="overlay owner eight alpha"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div id="owner_beta" class="overlay owner eight beta"><img src="./images/mic-bc-grfx-main-clear.png" /></div>
+                <div class="overlay foot">
+                    <img src="./images/mic-bc-grfx-main-clear.png" /></br>
+                    <div class="foot-cta"><a href="http://marketingincolor.com" target="_blank"><img src="./images/mic-bc-grfx-main-btyb.png" /></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="ea-modal" xid="dynamicContentID1" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+        <h2 id="modalTitle">Email Automation</h2>
+        <div id="dynamicContentID1">&nbsp;</div>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+    <div id="crm-modal" xid="dynamicContentID2" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+        <h2 id="modalTitle">CRM</h2>
+        <div id="dynamicContentID2">&nbsp;</div>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+    <div id="dwc-modal" xid="dynamicContentID3" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+        <h2 id="modalTitle">Dynamic Web Content</h2>
+        <div id="dynamicContentID3">&nbsp;</div>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+    <div id="de-modal" xid="dynamicContentID4" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+        <h2 id="modalTitle">Dynamic Email</h2>
+        <div id="dynamicContentID4">&nbsp;</div>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+    <div id="ct-modal" xid="dynamicContentID5" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+        <h2 id="modalTitle">Call Tracking</h2>
+        <div id="dynamicContentID5">&nbsp;</div>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+
+<?php include 'page-includes.php'; ?>
+
+<script>
+//Check String Length
+$( document ).ready(function() {
+    
+    // var namelength = result_b_sng.length;
+    // console.log(namelength);
+
+});
+</script>
+
+</body>
+</html>
